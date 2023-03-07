1bbdbd42ad66cc5a08812bd1a16556fc40e97f73
humblezxc <you@example.com>
2023-03-07 17:34:23 +0100

    Created endpoint to Send Messages

--- backend/src/controllers/Messages.ts
+++ backend/src/controllers/Messages.ts
@@ -9,3 +9,21 @@ export const getMessages = async (req: Request, res: Response): Promise<void> =>
         console.log(err);
     }
 };
+
+export const newMessage = async (req: Request, res: Response): Promise<void> => {
+    const { fullName, email, content } = req.body;
+    try {
+        const newMessage = await Message.create({ fullName, email, content });
+        if (newMessage) {
+            res.json({
+                message: 'Message sent successfully',
+                data: newMessage,
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Something went wrong',
+            data: {},
+        });
+    }
+};
