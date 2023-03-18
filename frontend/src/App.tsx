5be2f87d9380951c531dadec1ed4ce14d1fed8ba
humblezxc <you@example.com>
2023-03-18 18:48:05 +0100

    Connected scss and created glitched text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import AppRoutes from "./common/routes/AppRoutes";
 import Header from "./common/view/Header";
 import Footer from "./common/view/Footer";
-export default function App(){
-    return(
+
+export default function App() {
+    return (
         <BrowserRouter>
-            <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
-                <Header />
+            <div className="d-flex flex-column" style={{minHeight: "100vh"}}>
+                <Header/>
                 <div className="flex-grow-1 d-flex justify-content-center">
-                    <AppRoutes />
+                    <AppRoutes/>
                 </div>
-                <Footer />
             </div>
+            <Footer/>
+
         </BrowserRouter>
     );
 }
