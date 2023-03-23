2f1136fc321f5fed10e29e46956f938d8bcfbde3
humblezxc <you@example.com>
2023-03-23 15:41:56 +0100

    Created slide-up effect

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Button } from 'react-bootstrap';
 import ImageCards from "../content/ImageCards";
 
-export default function Home () {
-    return(
+export default function Home() {
+    return (
         <main>
-            <ImageCards />
+            <ImageCards/>
         </main>
     );
 }
