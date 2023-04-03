bffcb58f6f2041d42dc0d076b748d8701d41efff
humblezxc <you@example.com>
2023-04-04 00:09:00 +0200

    change bd file

--- backend/src/config/Database.ts
+++ backend/src/config/Database.ts
@@ -1,9 +1,10 @@
-import { Sequelize } from 'sequelize';
+import {Sequelize} from 'sequelize';
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const db = new Sequelize('new_schema'!, 'root'!, 'password'!, {
-    host: '127.0.0.1'!,
+const db = new Sequelize(process.env.DB_DATABASE!, process.env.DB_USERNAME!, process.env.DB_PASSWORD!, {
+    host: process.env.DB_HOST!,
     dialect: 'mysql'
 });
 db.authenticate()
