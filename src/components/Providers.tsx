9188f56ac2563e254b070f12380f6333937515a4
humblezxc <stopenchuk.yaroslav@gmail.com>
2026-01-22 23:15:44 +0100

    Add Vercel analytics

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,13 @@
 "use client";
 import React, { ReactNode } from "react";
 import { I18nProvider } from "@/lib/i18n";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-    return <I18nProvider>{children}</I18nProvider>;
+    return (
+        <I18nProvider>
+            {children}
+            <Analytics />
+        </I18nProvider>
+    );
 }
